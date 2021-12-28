cd6decfe4a626c60448453a1d564fbc4f1c45367
ShubinIgor <i.a.shubin.mail@gmail.com>
2021-12-28 13:24:01 +0300

    end of lesson 7

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
-
+  include ActionView::Helpers::TextHelper
+  
   def full_title(page_title = '')
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
